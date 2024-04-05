c1ada9dcb2dd9ad47eafc9108dc850f6949aa920
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-04-05 16:05:18 +0200

    fix

--- front/src/containers/NewBill.js
+++ front/src/containers/NewBill.js
@@ -43,11 +43,8 @@ export default class NewBill {
 
       this.document.querySelector(`input[data-testid="file"]`).value = "";
     } else {
-      /* istanbul ignore next */
       errorMessage.classList.remove("msg-block");
-      /* istanbul ignore next */
       errorMessage.style.display = "none";
-      /* istanbul ignore next */
       submitButton.disabled = false;
     }
     /* istanbul ignore next */
